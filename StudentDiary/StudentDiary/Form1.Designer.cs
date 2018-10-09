5da0531e38670bc77319c10a8e67f3a3a0d9e4ee
millhavenx666 <millhavenx666@gmail.com>
2018-10-09 23:42:47 +0200

    Dziennik uczniów: Wyswietlanie studentow z bazy

---
+++ StudentDiary/StudentDiary/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace StudentDiary
+{
+    partial class mainForm
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.studentButton = new System.Windows.Forms.Button();
+            this.studentListView1 = new System.Windows.Forms.ListView();
+            this.studentsColumnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.studentsColumnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // studentButton
+            // 
+            this.studentButton.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.studentButton.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.studentButton.Location = new System.Drawing.Point(23, 6);
+            this.studentButton.Name = "studentButton";
+            this.studentButton.Size = new System.Drawing.Size(83, 33);
+            this.studentButton.TabIndex = 2;
+            this.studentButton.Text = "Students";
+            this.studentButton.UseVisualStyleBackColor = false;
+            this.studentButton.Click += new System.EventHandler(this.studentButton_Click);
+            // 
+            // studentListView1
+            // 
+            this.studentListView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.studentsColumnHeader1,
+            this.studentsColumnHeader2});
+            this.studentListView1.Font = new System.Drawing.Font("Arial", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.studentListView1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.studentListView1.GridLines = true;
+            this.studentListView1.Location = new System.Drawing.Point(23, 45);
+            this.studentListView1.Name = "studentListView1";
+            this.studentListView1.Size = new System.Drawing.Size(176, 223);
+            this.studentListView1.TabIndex = 3;
+            this.studentListView1.UseCompatibleStateImageBehavior = false;
+            this.studentListView1.View = System.Windows.Forms.View.Details;
+            this.studentListView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // studentsColumnHeader1
+            // 
+            this.studentsColumnHeader1.Text = "Name";
+            this.studentsColumnHeader1.Width = 70;
+            // 
+            // studentsColumnHeader2
+            // 
+            this.studentsColumnHeader2.Text = "Surname";
+            this.studentsColumnHeader2.Width = 102;
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(409, 359);
+            this.Controls.Add(this.studentListView1);
+            this.Controls.Add(this.studentButton);
+            this.Name = "mainForm";
+            this.Text = "Student Diary";
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button studentButton;
+        private System.Windows.Forms.ListView studentListView1;
+        private System.Windows.Forms.ColumnHeader studentsColumnHeader1;
+        private System.Windows.Forms.ColumnHeader studentsColumnHeader2;
+    }
+}
+
---
+++ StudentDiary/StudentDiary/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace StudentDiary
+{
+    public partial class mainForm : Form
+    {
+
+        string connectionString;
+
+        SqlConnection sqlConnection;
+
+        public mainForm()
+        {
+            InitializeComponent();
+
+
+            connectionString = ConfigurationManager.ConnectionStrings["StudentDiary.Properties.Settings.StudentDiaryConnectionString"].ConnectionString;
+
+
+        }
+
+
+        private void studentButton_Click(object sender, EventArgs e)
+        {
+            populateStudents();
+        }
+        private void populateStudents()
+        {
+            using (sqlConnection = new SqlConnection(connectionString))
+            {
+                using (SqlDataAdapter sqlDataAdapter = new SqlDataAdapter("SELECT * FROM STUDENTS ORDER BY STUDENTS.Name", sqlConnection))
+                {
+
+
+                    DataTable studentsTable = new DataTable();
+                    sqlDataAdapter.Fill(studentsTable);
+
+                    foreach (DataRow dr in studentsTable.Rows)
+                    {
+                        ListViewItem item = new ListViewItem(dr["Name"].ToString());
+                        item.SubItems.Add(dr["Surname"].ToString());
+
+                        studentListView1.Items.Add(item);
+
+                    }
+
+
+                }
+            }
+            
+        }
+
+        private void mainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
